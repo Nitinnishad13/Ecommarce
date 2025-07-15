c1b7d8a4f0ef7b66508061eb44fdb0d654168c0d
Nitinnishad13 <nnishad13904@gmail.com>
2025-07-15 16:54:55 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import '../node_modules/bootstrap-icons/font/bootstrap-icons.css'
-import { AuthProvider } from './context/AuthContext.jsx'
+// import { AuthProvider } from './context/AuthContext.jsx'
+import { AuthProvider } from "./context/authContext.jsx";
 
 
 createRoot(document.getElementById('root')).render(
